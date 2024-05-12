3b673cd1a2bce8ef1dcc6fc19db551a357d36e06
lucanotluka <luca.gritti02@gmail.com>
2024-05-12 16:20:30 +0200

    Added libs, NOW REGISTRATION WORKS!!

--- src/main/java/it/polimi/tiw/controllers/RegisterController.java
+++ src/main/java/it/polimi/tiw/controllers/RegisterController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringEscapeUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import it.polimi.tiw.beans.User;
 import it.polimi.tiw.dao.UserDAO;
@@ -20,7 +25,9 @@ import it.polimi.tiw.utils.ConnectionHandler;
 public class RegisterController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Connection connection = null;
-
+	 private TemplateEngine templateEngine;
+	
+	
     public RegisterController() {
         super();
     }
@@ -28,12 +35,12 @@ public class RegisterController extends HttpServlet {
 
 	public void init() throws ServletException {
 		connection = ConnectionHandler.getConnection(getServletContext());
-//		ServletContext servletContext = getServletContext();
-//		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
-//		templateResolver.setTemplateMode(TemplateMode.HTML);
-//		this.templateEngine = new TemplateEngine();
-//		this.templateEngine.setTemplateResolver(templateResolver);
-//		templateResolver.setSuffix(".html");
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -45,13 +52,13 @@ public class RegisterController extends HttpServlet {
         String password = null;
         String confirmPassword = null;
 		
-		try { // StringEscapeUtils.escapeJava();
-			username = request.getParameter("username");
-			email = request.getParameter("email");
-			name = request.getParameter("name");
-			surname = request.getParameter("surname");
-			password = request.getParameter("password");
-			confirmPassword = request.getParameter("confirmPassword");
+		try {
+			username = StringEscapeUtils.escapeJava(request.getParameter("username"));
+			email = StringEscapeUtils.escapeJava(request.getParameter("email"));
+			name = StringEscapeUtils.escapeJava(request.getParameter("name"));
+			surname = StringEscapeUtils.escapeJava(request.getParameter("surname"));
+			password = StringEscapeUtils.escapeJava(request.getParameter("password"));
+			confirmPassword = StringEscapeUtils.escapeJava(request.getParameter("confirmPassword"));
 			
 			if (username == null || name == null || surname == null || email == null || password == null || confirmPassword == null 
 					|| username.isEmpty() || name.isEmpty() || surname.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
@@ -89,15 +96,14 @@ public class RegisterController extends HttpServlet {
    			success = userDao.registerSuccess(user);
    			
     		if(success) {
+    			
     			// REGISTRATION SUCCESS message
     			
-//    			ServletContext servletContext = getServletContext();
-//    			final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-//    			ctx.setVariable("errorMsg", "Incorrect username or password");
-//    			path = "/LandingPage.html";
-//    			templateEngine.process(path, ctx, response.getWriter());
-    			
-    			response.sendRedirect("/LandingPage.html");
+    			ServletContext servletContext = getServletContext();
+    			final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+    			ctx.setVariable("successMsg", "Registration correctly done!");
+    			String path = "/LandingPage.html";
+    			templateEngine.process(path, ctx, response.getWriter());
     		}
     		
     		// if not successful, throws sql exception
